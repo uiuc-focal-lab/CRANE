cb50960275a522f2fbc8de527954ebee7ba4c01b
Tarun Suresh <tarsur909@gmail.com>
2026-01-10 08:44:15 -0500

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-pip uninstall -y opencv-python opencv-python-headless opencv-contrib-python jax jaxlib shap pytensor
-pip uninstall -y omegaconf hydra-core  # if you don't need them
-
-pip install --upgrade --force-reinstall --no-cache-dir numpy==1.26.4
-pip install --upgrade --force-reinstall --no-cache-dir antlr4-python3-runtime==4.11.1
-
 
 cd syncode/
 pip install -e .
